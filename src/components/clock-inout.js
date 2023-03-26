0e0f2d569fee0c8bd836746a7d8da7c5b9f677ff
I Komang Sena <i.buwana@btpn.com>
2023-03-26 10:06:47 +0700

    fix: clockin default date time

--- src/components/clock-inout.js
+++ src/components/clock-inout.js
@@ -4,34 +4,49 @@ import {
 } from 'antd';
 
 const _findLastAbsence = (absences = []) => {
-  const lastClockIn = absences.find((absence) => absence?.status === 'CLOCK_IN');
-  const lastClockOut = absences.find((absence) => absence?.status === 'CLOCK_OUT');
-
-  return [lastClockIn?.time, lastClockOut?.time];
-};
-
-function ClockInOut({ absences, onClockIn, onClockOut }) {
-  const [clockIn, clockOut] = _findLastAbsence(absences);
   const dateFormat = 'd MMM yyyy';
   const timeFormat = 'HH:mm';
 
+  const lastClockIn = absences.find((absence) => absence?.status === 'CLOCK_IN');
+  const lastClockOut = absences.find((absence) => absence?.status === 'CLOCK_OUT');
+
+  const todayDate = format(new Date(), dateFormat);
+  const clockInDate = lastClockIn?.time
+    ? format(new Date(lastClockIn?.time), dateFormat)
+    : todayDate;
+  const clockOutDate = lastClockOut?.time
+    ? format(new Date(lastClockOut?.time), dateFormat)
+    : todayDate;
+  const clockInTime = lastClockIn?.time ? format(new Date(lastClockIn?.time), timeFormat) : '-';
+  const clockOutTime = lastClockOut?.time ? format(new Date(lastClockOut?.time), timeFormat) : '-';
+
+  return {
+    clockInDate, clockOutDate, clockInTime, clockOutTime,
+  };
+};
+
+function ClockInOut({ absences, onClockIn, onClockOut }) {
+  const {
+    clockInDate, clockOutDate, clockInTime, clockOutTime,
+  } = _findLastAbsence(absences);
+
   return (
     <>
-      <Row span={16} justify="center">
-        <Col span={8}>
-          <Typography.Text>{format(new Date(clockIn), dateFormat)}</Typography.Text>
-          <Typography.Title level={3}>{format(new Date(clockIn), timeFormat)}</Typography.Title>
+      <Row span={10} justify="center">
+        <Col span={5}>
+          <Typography.Text>{clockInDate}</Typography.Text>
+          <Typography.Title level={3}>{clockInTime}</Typography.Title>
         </Col>
-        <Col span={8}>
-          <Typography.Text>{format(new Date(clockOut), dateFormat)}</Typography.Text>
-          <Typography.Title level={3}>{format(new Date(clockOut), timeFormat)}</Typography.Title>
+        <Col span={5}>
+          <Typography.Text>{clockOutDate}</Typography.Text>
+          <Typography.Title level={3}>{clockOutTime}</Typography.Title>
         </Col>
       </Row>
-      <Row span={16} justify="center">
-        <Col span={8}>
+      <Row span={10} justify="center">
+        <Col span={5}>
           <Button type="primary" onClick={onClockIn}>Clock In</Button>
         </Col>
-        <Col span={8}>
+        <Col span={5}>
           <Button type="primary" onClick={onClockOut}>Clock Out</Button>
         </Col>
       </Row>
